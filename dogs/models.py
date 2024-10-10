dbbc3a01ab6f0f5dd4bf6ff8c732ab707d1d9b65
Zheldakov Andrey <zheldakov.aa@gmail.com>
2024-10-10 15:32:32 +0300

    Добавлена модель Dogs

--- dogs/models.py
+++ dogs/models.py
@@ -4,11 +4,23 @@ from django.db import models
 NULLABLE ={'blank': True, 'null': True}
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, verbose_name='breed', **NULLABLE)
-    description = models.CharField(max_length=150, verbose_name='description', **NULLABLE)
+    name = models.CharField(max_length=100, verbose_name='breed',**NULLABLE) #`имя породы (категории)
+    description = models.CharField(max_length=150, verbose_name='description',**NULLABLE) # описание породы
     def __str__(self):
         return self.name
 
     class Meta:
         verbose_name = 'breed'
         verbose_name_plural = 'breeds'
+
+
+class Dogs(models.Model):
+    name = models.CharField(max_length=250, verbose_name='dog_name',**NULLABLE)  # имя собаки
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='breed') # порода собаки
+    photo = models.ImageField(upload_to='dogs/', verbose_name='image', **NULLABLE) # фотография собаки
+    birth_date = models.DateField(verbose_name='birth_date',**NULLABLE) # дата рождения собаки
+    def __str__(self):
+        return f'{self.name} ({self.category})' # собака в формате "Имя собаки (порода)"
+    class Meta:
+        verbose_name = 'dog' # собака
+        verbose_name_plural = 'dogs' # собаки
