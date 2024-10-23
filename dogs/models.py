8274ddc0557702ab88504a3719fd732b0c80312f
Zheldakov Andrey <zheldakov.aa@gmail.com>
2024-10-23 15:54:28 +0300

    Добавлено поле владельца собаки

--- dogs/models.py
+++ dogs/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.conf import settings
 # настройка полей, чтобы возможно было заполнить поля пустыми
 NULLABLE ={'blank': True, 'null': True}
 
@@ -19,6 +19,9 @@ class Dog(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='breed') # порода собаки
     photo = models.ImageField(upload_to='dogs/', verbose_name='image', **NULLABLE) # фотография собаки
     birth_date = models.DateField(verbose_name='birth_date',**NULLABLE) # дата рождения собаки
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE,
+                              verbose_name='владелец')
     def __str__(self):
         return f'{self.name} ({self.category})' # собака в формате "Имя собаки (порода)"
     class Meta:
