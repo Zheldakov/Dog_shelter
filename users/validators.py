ef462eed6237f87fbbe88edae2f47c17bda8d8f0
Zheldakov Andrey <zheldakov.aa@gmail.com>
2024-10-23 16:12:34 +0300

    Исправлено количество минимальных знаков пароля

--- users/validators.py
+++ users/validators.py
@@ -7,6 +7,6 @@ def validate_password(field):
     if not bool(re.match(pattern, field)):
         print('Must contain A-Z, a-z letters and 0-9 numbers')
         raise ValidationError('Пароль должен содержать символы латинского алфавита и цифры!')
-    if not 6 <= len(field) <= 12:
+    if not 8 <= len(field) <= 12:
         print('Password length must be between 8 and 12 characters')
         raise ValidationError('Длина пароля должна быть между 8 и 12 символов')
