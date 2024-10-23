0dab2a376abad9b3a04ae52fc1df9fbb5fbe8b53
Zheldakov Andrey <zheldakov.aa@gmail.com>
2024-10-23 09:37:26 +0300

    Добавлен михсин в форму собаки

--- dogs/forms.py
+++ dogs/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 
 from dogs.models import Dog
+from users.forms import StyleFromMixin
 
-class DogForm(forms.ModelForm):
+class DogForm(StyleFromMixin,forms.ModelForm):
     # Создаем форму для добавления/редактирования питомца с указанными полями
     class Meta:
         model = Dog
